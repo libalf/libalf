b5925a503ac9ed476954a631ab0c23a49391ce3a
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2008-11-04 00:24:48 +0000

    makefiles
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@123 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- testsuites/angluin_simple_observationtable/test.sh
+++ testsuites/angluin_simple_observationtable/test.sh
@@ -9,9 +9,9 @@ cat regex_sample_set.txt |
 		echo $NUM: $REGEX;
 		MY_MINSTATES=` ./learn_regex $REGEX | grep 'minimal state count:' | gawk '{print $4}'; `
 		if [ "$MINSTATES" != "$MY_MINSTATES" ]; then
-			echo $NUM: $REGEX >> log
-			echo "fail: minimal state count differs: org: $MINSTATES, new: $MY_MINSTATES" >> log
-			echo "" >> log
+			echo $NUM: $REGEX >> testlog
+			echo "fail: minimal state count differs: org: $MINSTATES, new: $MY_MINSTATES" >> testlog
+			echo "" >> testlog
 		fi;
 		let NUM++;
 	done;
