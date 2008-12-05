0a301d6d4e78d6e0ddb55307eca3d99afb4b9037
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2008-12-05 23:06:54 +0000

    blank files for SQT testsuites
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@166 ee1db7db-6c55-0410-a809-e4d5348c71a5

---
+++ testsuites/structured_query_tree/sqt_learn_regex_angluin.cpp
@@ -0,0 +1,23 @@
+/* $Id$
+ * vim: fdm=marker
+ *
+ * libalf - Automata Learning Factory
+ *
+ * (c) by David R. Piegdon, i2 Informatik RWTH-Aachen
+ *        <david-i2@piegdon.de>
+ *
+ * see LICENSE file for licensing information.
+ */
+
+#include <iostream>
+#include <iterator>
+#include <fstream>
+
+#include "libalf/alf.h"
+
+#include <amore/vars.h>
+
+
+using namespace std;
+using namespace libalf;
+
