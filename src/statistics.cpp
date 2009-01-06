ecb8498851710dfa1e14227f2284dce533aa0298
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-01-06 12:43:53 +0000

    deserializing stats
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@270 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -53,9 +53,40 @@ basic_string<int32_t> statistics::serialize()
 }}}
 
 bool statistics::deserialize(basic_string<int32_t>::iterator & it, basic_string<int32_t>::iterator final)
-{
-	// FIXME: deserialize is not implemented
+{{{
+	int size;
+
+	if(it == limit)
+		goto deserialization_failed;
+
+	// data size
+	size = ntohl(*it);
+
+	// table_size.bytes
+	it++; if(size <= 0 || limit == it) goto deserialization_failed;
+	table_size.bytes = ntohl(*it);
+	// table_size.members
+	it++; size--; if(size <= 0 || limit == it) goto deserialization_failed;
+	table_size.members = ntohl(*it);
+	// table_size.words
+	it++; size--; if(size <= 0 || limit == it) goto deserialization_failed;
+	table_size.words = ntohl(*it);
+
+	// query_count.membership
+	it++; size--; if(size <= 0 || limit == it) goto deserialization_failed;
+	query_count.membership = ntohl(*it);
+	// query_count.equivalence
+	it++; size--; if(size <= 0 || limit == it) goto deserialization_failed;
+	query_count.equivalence = ntohl(*it);
+
+	it++; size--;
+
+	if(size == 0)
+		return true;
+
+deserialization_failed:
+	reset();
 	return false;
-}
+}}}
 
 }; // end namespace libalf
