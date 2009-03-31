9ee4a4e7a8da68e34f9f2bf08a60cb7f2b34a0c2
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-03-31 18:11:18 +0000

    example stuff
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@425 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- testsuites/biermann/biermann_test.cpp
+++ testsuites/biermann/biermann_test.cpp
@@ -80,18 +80,19 @@ int main(int argc, char**argv)
 */
 	// FIXME:
 	// create sample set in knowledgebase
-#if 1
+#if 0
 	{
 		// for now, just add some samples...
 		alphabet_size = 2;
 		list<int> w;
-		w.push_back(0);
-		w.push_back(0);
-		knowledge.add_knowledge(w, true);
-		w.pop_back();
-		knowledge.add_knowledge(w, true);
-		w.pop_back();
 		knowledge.add_knowledge(w, false);
+		w.push_back(0);
+		knowledge.add_knowledge(w, true);
+		w.push_back(0);
+		knowledge.add_knowledge(w, true);
+
+		w.pop_back();
+		w.pop_back();
 		w.push_back(1);
 		knowledge.add_knowledge(w, false);
 		w.push_back(0);
@@ -107,11 +108,13 @@ int main(int argc, char**argv)
 		knowledge.add_knowledge(w, false);
 		w.push_back(1);
 		knowledge.add_knowledge(w, true);
+
 		w.pop_back();
 		w.push_back(0);
 		knowledge.add_knowledge(w, false);
 		w.push_back(0);
 		knowledge.add_knowledge(w, true);
+
 		w.pop_back();
 		w.pop_back();
 		w.pop_back();
