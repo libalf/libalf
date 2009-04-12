c277c01a30a53f6c8de6fb9f06531d583b2d17cf
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-04-12 16:44:31 +0000

    amore++ bits
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@461 ee1db7db-6c55-0410-a809-e4d5348c71a5

---
+++ libAMoRE++/src/generic.cpp
@@ -0,0 +1,29 @@
+/* $Id$
+ * vim: fdm=marker
+ *
+ * amore++
+ *
+ * (c) by David R. Piegdon, i2 Informatik RWTH-Aachen
+ *        <david-i2@piegdon.de>
+ *
+ * see LICENSE file for licensing information.
+ */
+
+#include <amore/ext.h>
+#include <amore/buffer.h>
+// <amore/vars> defines specific vars required by libAMoRE-1.0,
+// like the alphabet
+#include <amore/vars.h>
+
+void _init()
+{
+	// initialize libAMoRE buffers
+	initbuf();
+}
+
+void _fini()
+{
+	// free libAMoRE buffers
+	freebuf();
+}
+
