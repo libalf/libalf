eda90fee1db6466f8b7461759da6a64c30c7ec7c
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-04-15 09:52:00 +0000

    JNI first check-in
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@474 ee1db7db-6c55-0410-a809-e4d5348c71a5

---
+++ JNI/jni_knowledgebase.cpp
@@ -0,0 +1,156 @@
+#include <libalf/automaton_constructor.h>
+#include <libalf/knowledgebase.h>
+#include <iostream>
+#include <jni.h>
+#include "jni_knowledgebase.h"
+
+using namespace std;
+using namespace libalf;
+
+jintArray makeJavaIntArray(JNIEnv *env, list<int> l) {
+	// Create new Java int array
+	jintArray arr = env->NewIntArray(l.size());
+
+	// Copy array
+	int intArray[l.size()];
+	int i=0;
+	list<int>::iterator li;
+	for(li = l.begin(); li != l.end(); li++) {
+		intArray[i] = *li;
+		i++;
+	}
+
+	// Fill Java array
+	env->SetIntArrayRegion(arr, 0, l.size(), (jint *)intArray);
+
+	return arr;
+}
+
+JNIEXPORT jint JNICALL Java_Knowledgebase_init (JNIEnv *env, jobject obj) {
+	/*
+	 * Return the new object
+	 */
+	knowledgebase<bool> *base = new knowledgebase<bool>;
+	return ((jint)base);
+}
+
+JNIEXPORT jboolean JNICALL Java_Knowledgebase_is_1answered (JNIEnv *env, jobject obj, jint pointer) {
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+
+	// Forward method call
+	return base->is_empty();
+}
+
+JNIEXPORT jboolean JNICALL Java_Knowledgebase_is_1empty (JNIEnv *env, jobject obj, jint pointer) {
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+
+	// Forward method call
+	return base->is_answered();
+}
+
+JNIEXPORT jint JNICALL Java_Knowledgebase_count_1queries (JNIEnv *env, jobject obj, jint pointer) {
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+
+	// Forward method call
+	return base->count_queries();
+}
+
+JNIEXPORT jobject JNICALL Java_Knowledgebase_getQueries (JNIEnv *env, jobject obj, jint pointer) {
+	/*
+	 *Create new Java LinkedList object
+	 */
+	// Find class
+	jclass jcls = env->FindClass("LibALFQueries");
+	if(jcls == NULL) {
+		cout << "Could not find Java Class 'LinkedList'!\nReturning NULL\n";
+		return NULL;
+	}
+	// Find constructor
+	jmethodID jmid = env->GetMethodID(jcls, "<init>", "()V");
+	if(jmid == NULL) {
+		cout << "Could not find constructor of 'LinkedList'!\nReturning NULL\n";
+		return NULL;
+	}
+	// Make new object
+	jobject java_queries = env->NewObject(jcls, jmid);
+	if(jmid == NULL) {
+		cout << "Could not create new 'LinkedList' object!\nReturning NULL\n";
+		return NULL;
+	}
+	
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+
+	/*
+	 * Fill list
+	 */
+	knowledgebase<bool>::iterator ki;
+
+	// Find add method
+	//jmid = env->GetMethodID(jcls, "add", "(Ljava/lang/Object;)Z ");
+	jmid = env->GetMethodID(jcls, "jniAdd", "([I)V");
+	if(jmid == NULL) {
+		cout << "Could not find add method of 'LinkedList'!\nReturning NULL\n";
+		return NULL;
+	}
+	// Loop through all queries
+	for(ki = base->qbegin(); ki != base->qend(); ki++) {
+		// Convert list to a Java array
+		jintArray arr = makeJavaIntArray(env, ki->get_word());
+
+		/*
+		 * Add the array to the linked list
+		 */
+		env->CallVoidMethod(java_queries, jmid, arr);
+
+	}
+
+	return java_queries;
+}
+
+JNIEXPORT jintArray JNICALL Java_Knowledgebase_testIntArray (JNIEnv *env, jobject obj, jint size) {
+	  list<int> list;
+	int s = size;
+	  for(int i=0; i<s; i++) list.push_back(i);
+
+	return makeJavaIntArray(env, list);
+}
+
+JNIEXPORT jboolean JNICALL Java_Knowledgebase_resolve_1query (JNIEnv *env, jobject obj, jintArray word, jboolean acceptance, jint pointer) {
+	// Get Java array info
+	jsize length = env->GetArrayLength(word);
+	jint *entry = env->GetIntArrayElements(word, 0);
+	// Copy array
+	int len = (int)length;
+	list<int> w;
+	for(int i=0; i<len; i++) w.push_back(((jint)entry[i]));
+	// Clean
+	env->ReleaseIntArrayElements(word, entry, 0);
+
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+	// Forward method call
+	bool acc = acceptance;
+	return base->resolve_query(w, acc);
+}
+
+JNIEXPORT jboolean JNICALL Java_Knowledgebase_add_1knowledge (JNIEnv *env, jobject obj, jintArray word, jboolean acceptance, jint pointer) {
+	// Get Java array info
+	jsize length = env->GetArrayLength(word);
+	jint *entry = env->GetIntArrayElements(word, 0);
+	// Copy array
+	int len = (int)length;
+	list<int> w;
+	for(int i=0; i<len; i++) w.push_back(((jint)entry[i]));
+	// Clean
+	env->ReleaseIntArrayElements(word, entry, 0);
+
+	// Get the knowledgebase object
+	knowledgebase<bool> *base = (knowledgebase<bool>*)pointer;
+	// Forward method call
+	bool acc = acceptance;
+	return base->add_knowledge(w, acc);
+}
