3354e55b9a446422d0e1ec3b8e024d6abb760083
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-05-05 23:22:30 +0000

    cleanup testcase
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@587 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- LanguageGenerator/testsuites/regex_randomgenerator/regex_randomgenerator.cpp
+++ LanguageGenerator/testsuites/regex_randomgenerator/regex_randomgenerator.cpp
@@ -32,7 +32,8 @@ int main(int argc, char**argv)
 	bool show_mregex = false;
 
 	if(argc != 3 && argc != 4 || (argc == 4 && 0 != strcmp(argv[1], "-r")) ) {
-		cout << "please give <alphabet size> and <operand count> as parameters, possibly prefixed by --rex\n";
+		cout << "please give <alphabet size> and <operand count> as parameters,\n"
+			"possibly prefixed by -r to see the regex derived from the mDFA\n";
 		return 1;
 	}
 
