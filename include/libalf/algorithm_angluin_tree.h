26bb33155c8a33ff44e5df3f1859db9db21cf3db
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-05-23 20:33:48 +0000

    L* tree bits
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@612 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- include/libalf/algorithm_angluin_tree.h
+++ include/libalf/algorithm_angluin_tree.h
@@ -34,128 +34,116 @@ template <class answer>
 class angluin_tree : public learning_algorithm<answer> {
 	/* in learning_algorithm: *my_logger, *my_knowledge, *norm, alphabet_size */
 	protected: // types
-		typedef pair<bool, knowledgebase::node*> candidate;
-		// <true, X> if it is a state-candidate (would be in upper table),
-		// <false, X> if it is a transition-candidate (would be in lower)
+		class node;
+
+		class candidate {
+			public:
+				typename knowledgebase<answer>::node * word;
+				node * position; // position in tree
+				bool is_state; // state candidate or just a transition?
+			public:
+				candidate(typename knowledgebase<answer>::node *word, node *position, bool is_state)
+				{{{
+					this->word = word;
+					this->position = position;
+					this->is_state = is_state;
+				}}}
+				void make_state()
+				{{{
+					is_state = true;
+				}}}
+		};
 
 		class node {
 			public:
-				list<int> suffix;
+				list<int> suffix; // splitting suffix of this node
 				bool is_leaf;
-				list<candidate> pending;
+				list<candidate *> candidates; // candidates (pending, if this is not a leaf)
 				node *accepting, *rejecting;
-
+			public:
 				node()
-				{ is_leaf = false; accepting = NULL; rejecting = NULL; };
-
-				~node()
-				{ if(!is_leaf) { delete accepting; delete rejecting; } };
-
-				void make_internal(list<int> & suffix);
 				{{{
-					if(is_leaf) {
-						this->suffix = suffix;
-						accepting = new node;
-						rejecting = new node;
+					is_leaf = false;
+					accepting = NULL;
+					rejecting = NULL;
+				}}};
+				~node()
+				{{{
+					if(!is_leaf) {
+						delete accepting;
+						delete rejecting;
 					}
 				}}}
-
 				int get_memory_usage()
 				{{{
 					int ret = sizeof(this);
 					if(!is_leaf) {
-						ret += accepting->get_memory_statistics();
-						ret += rejecting->get_memory_statistics();
+						ret += accepting->get_memory_usage();
+						ret += rejecting->get_memory_usage();
 					}
 					return ret;
-				}}}
-
-				void count(int & suffixes, int & state_candidates, int & transition_candidates, bool initial_recursion = true)
-				{{{
-					list<candidate>::iterator ci;
-
-					if(initial_recursion) {
-						suffixes = 0;
-						state_candidates = 0;
-						transition_candidates = 0;
-					}
-
-					for(ci = pending.begin(); ci != pending.end(); ++ci)
-						if(ci->first)
-							state_candidates++;
-						else
-							transition_candidates++;
-
-					if(is_leaf) {
-						suffixes++;
-					} else {
-						accepting->count(suffixes, state_candidates, transition_candidates, false);
-						rejecting->count(suffixes, state_candidates, transition_candidates, false);
-					}
-				}}}
-
-				int update(knowledgebase * base)
-				// update tree: try to move pending downwards.
-				// returns number of pending candidates NOT in a leaf
-				{{{
-					if(is_leaf) {
-						return 0;
-					} else {
-						int internal_candidates = 0;
-						list<candidate>::iterator ci;
-
-						ci = pending.begin();
-						while(ci != pending.end()) {
-							// try to move down by querying knowledgebase
-							knowledgebase::node * q;
-							q = ci->second->find_or_create_child(suffix.begin(), suffix.end());
-							if(q->is_answered()) {
-								if(q->get_answer() == true)
-									accepting->pending.push_back(*ci);
-								else
-									rejecting->pending.push_back(*ci);
-
-								ci = pending.erase(ci);
-							} else {
-								q->mark_required();
-								internal_candidates++;
-
-								ci++;
-							}
-						}
-
-						internal_candidates += accepting->update(base);
-						internal_candidates += rejecting->update(base);
-
-						return internal_candidates;
-					}
 				}}}
 		};
 
 	protected: // data
-		node * tree;
+		node * tree; // suffix-splitting tree
+
+		list<candidate*> pending; // all pending candidates (that have not yet reached a leaf in the tree)
+
+		map<typename knowledgebase<answer>::node*, candidate*> candidates; // ALL candidates
+		set<candidate*> final; // NOTE: also contains transitions going to final states. this is maintained in sift_pending().
 
 	public:
 		angluin_tree()
-		{ tree = NULL; }
+		{{{
+			tree = NULL;
+			this->set_alphabet_size(0);
+			this->set_knowledge_source(NULL);
+			this->set_logger(NULL);
+		}}}
+		angluin_tree(knowledgebase<answer> *base, logger *log, int alphabet_size)
+		{{{
+			tree = NULL;
+			this->set_alphabet_size(alphabet_size);
+			this->set_knowledge_source(base);
+			this->set_logger(log);
+		}}}
 		virtual ~angluin_tree()
 		{{{
+			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
+
 			if(tree)
 				delete tree;
+			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
+				delete ci->second;
 		}}}
+
+		virtual void new_candidate(typename knowledgebase<answer>::node* word, bool is_state)
+		{{{
+			candidate * c = new candidate(word, tree, is_state);
+			tree->candidates.push_back(c);
+			candidates[word] = c;
+		}}}
+
 		virtual void increase_alphabet_size(int new_asize)
-		{
-		}
+		{{{
+			if(new_asize <= this->get_alphabet_size())
+				return;
+
+			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
+
+			// add new transitions for all states.
+			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
+				for(int new_suffix = this->get_alphabet_size(); new_suffix < new_asize; ++new_suffix)
+					new_candidate(ci->second->word->find_or_create_child(new_suffix), false);
+
+			this->set_alphabet_size(new_asize);
+		}}}
 		virtual void get_memory_statistics(statistics & stats)
 		// table_size.words is an approximation!
-		{{{
-			stats.table_size.bytes = sizeof(this);
-			if(tree) {
-				stats.table_size.bytes += tree->get_memory_usage();
-				tree->count(stats.table_size.columns, stats.table_size.upper_table, stats.table_size.lower_table);
-				stats.table_size.words = (stats.table_size.upper_table + stats.table_size.lower_table) * log(stats.table_size.columns) / log(2);
-			}
-		}}}
+		{
+			
+		}
 		virtual bool sync_to_knowledgebase()
 		{{{
 			(*this->my_logger)(LOGGER_ERROR, "angluin_tree: sync-operation not supported but trying to sync!\n");
@@ -165,41 +153,141 @@ class angluin_tree : public learning_algorithm<answer> {
 		{ return false; }
 		virtual basic_string<int32_t> serialize()
 		{
+			
 		}
 		virtual bool deserialize(basic_string<int32_t>::iterator &it, basic_string<int32_t>::iterator limit)
 		{
+			
 		}
 		virtual void print(ostream &os)
 		{ os << tostring(); }
 		virtual string tostring()
 		{
+			
 		}
 		virtual bool conjecture_ready()
 		{
+			if(pending.size() > 0)
+				return false;
+			// check consistency and closedness
+			
 		}
 		virtual void add_counterexample(list<int>)
 		{
+			
 		}
 
 	protected:
-		virtual bool complete()
+		bool sift_pending()
+		{{{
+			bool new_required = false;
+			if(pending.size() == 0)
+				return true;
+
+			typename list<candidate*>::iterator pi;
+
+			for(pi = pending.begin(); pi != pending.end(); ++pi) {
+				typename knowledgebase<answer>::node *w;
+				bool sift_done = (*pi)->position->is_leaf;
+
+				while(!sift_done) {
+					// get word from prefix and new suffix
+					w = (*pi)->word->find_or_create_child((*pi)->position->suffix.begin(), (*pi)->position->suffix.end());
+					if(w->is_answered()) {
+						// move candidate to subtree
+						typename list<candidate*>::iterator d;
+						for(d = (*pi)->position->candidates.begin(); d != (*pi)->position->candidates.end(); ++d)
+							if(*d == *pi)
+								break;
+						(*pi)->position->candidates.erase( d );
+
+						// mark final-status now.
+						if((*pi)->position == tree)
+							if(w->get_answer() == true)
+								final.insert(*pi);
+
+						if(w->get_answer() == true)
+							(*pi)->position = (*pi)->position->accepting;
+						else
+							(*pi)->position = (*pi)->position->rejecting;
+
+						(*pi)->position->candidates.push_back(*pi);
+						if((*pi)->position->is_leaf) {
+							sift_done = true;
+							pi = pending.erase(pi);
+						};
+					} else {
+						// unable to sift until we're at a leaf.
+						// a teacher needs to answer the queries in the knowledgebase.
+						w->mark_required();
+						new_required = true;
+						sift_done = true;
+					}
+				}
+			}
+
+			return new_required;
+		}}}
+
+		bool is_closed()
 		{
-			if(tree->update == 0) {
+			// for each non-state candidate check if there is a state-candidate at the same leaf.
+			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
+
+			// create a set with all transition-candidates
+			set<candidate*> transitions;
+			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
+				if(!(*ci)->is_state)
+					transitions.insert(*ci);
+
+			// for all state-candidates, remove transitions going into them.
+			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
+				if((*ci)->is_state) {
+					typename list<candidate*>::iterator pi;
+					for(pi = (*ci)->position->candidates.begin(); pi != (*ci)->position->candidates.end(); ++pi)
+						transitions.erase(*pi);
+				}
+
+			// if there is a remainder, the table is not closed.
+			return transitions.empty();
+		}
+		bool close()
+		{
+			// for each non-state candidate check if there is a state-candidate at the same leaf.
+			// if not, make it a state-candidate.
+			
+		}
+
+		bool is_consistent()
+		{
+			// check if for all candidates at same leaf their transitions go to the same leaves.
+			
+		}
+		bool make_consistent()
+		{
+			// check if for all candidates at same leaf their transitions go to the same leaves.
+			// if not, find splitting suffix and add it at the current leaf.
+			
+		}
+
+		virtual bool complete()
+		{{{
+			if(sift_pending()) {
 				if(!close())
 					return complete();
-				if(!make_consistent)
+				if(!make_consistent())
 					return complete();
 
 				return true;
 			} else {
 				return false;
 			}
-		}
+		}}}
+
 		virtual bool derive_automaton(bool & is_dfa, int & alphabet_size, int & state_count, set<int> & initial, set<int> & final, multimap<pair<int, int>, int> & transitions)
 		{
+			
 		}
-
-
 
 };
 
---
+++ testsuites/angluin_tree/learn_regex.cpp
@@ -0,0 +1,214 @@
+/* $Id$
+ * vim: fdm=marker
+ *
+ * libalf - Automata Learning Factory
+ *
+ * (c) by David R. Piegdon, i2 Informatik RWTH-Aachen
+ *        <david-i2@piegdon.de>
+ *
+ * see LICENSE file for licensing information.
+ */
+
+#include <iostream>
+#include <ostream>
+#include <iterator>
+#include <fstream>
+#include <algorithm>
+
+#include <libalf/alf.h>
+#include <libalf/algorithm_angluin_tree.h>
+
+#include <amore++/nondeterministic_finite_automaton.h>
+
+#include "amore_alf_glue.h"
+
+//#define ANSWERTYPE extended_bool
+#define ANSWERTYPE bool
+
+using namespace std;
+using namespace libalf;
+using namespace amore;
+
+int main(int argc, char**argv)
+{
+	statistics stats;
+
+	finite_automaton *nfa;
+	ostream_logger log(&cout, LOGGER_DEBUG);
+
+	knowledgebase<ANSWERTYPE> knowledge;
+
+	char filename[128];
+	ofstream file;
+
+	int iteration;
+	bool success = false;
+
+	int alphabet_size;
+	int hypothesis_state_count = 0;
+
+	bool regex_ok;
+	if(argc == 3) {
+		nfa = new nondeterministic_finite_automaton(atoi(argv[1]), argv[2], regex_ok);
+	} else /* find alphabet size or show some example regex */ {{{
+		if(argc == 2) {
+			nfa = new nondeterministic_finite_automaton(argv[1], regex_ok);
+		} else {
+			cout << "either give a sole regex as parameter, or give <alphabet size> <regex>.\n\n";
+			cout << "example regular expressions:\n";
+			cout << "alphabet size, \"regex\":\n";
+			cout << "2 '((a((aa)a))U(((bb))*((((bU(ab))U(bUa)))*)*))'\n";
+			cout << "2 '(((bb)|a)(b(((bb)b)(((aa)a)|a))))'\n";
+			cout << "2 '(((aa)(a)*)(((a((b(b)*)(aUb)))((ba))*))*)'\n";
+			cout << "3 '(cbb(ab(c)*))* U (a((cbb*) U a+b+bc)+)'\n";
+			return 1;
+		}
+	}}}
+
+	if(regex_ok) {
+		log(LOGGER_INFO, "REGEX ok.\n");
+	} else {
+		log(LOGGER_ERROR, "REGEX failed.\n");
+		return 1;
+	}
+
+	alphabet_size = nfa->get_alphabet_size();
+
+	{{{ /* dump original automata */
+		file.open("original-nfa.dot"); file << nfa->generate_dotfile(); file.close();
+
+		finite_automaton * dfa;
+		dfa = nfa->determinize();
+		dfa->minimize();
+		file.open("original-dfa.dot"); file << dfa->generate_dotfile(); file.close();
+		delete dfa;
+	}}}
+
+
+	// create oracle instance and teacher instance
+	knowledge.set_statistics(&stats);
+
+	// create angluin_simple_table and teach it the automaton
+	angluin_tree<ANSWERTYPE> ot(&knowledge, &log, alphabet_size);
+	finite_automaton * hypothesis = NULL;
+
+	for(iteration = 1; iteration <= 100; iteration++) {
+		int c = 'a';
+		bool f_is_dfa;
+		int f_alphabet_size, f_state_count;
+		set<int> f_initial, f_final;
+		multimap<pair<int, int>, int> f_transitions;
+
+		while( ! ot.advance(f_is_dfa, f_alphabet_size, f_state_count, f_initial, f_final, f_transitions) ) {
+			// resolve missing knowledge:
+
+			snprintf(filename, 128, "knowledgebase%02d%c.dot", iteration, c);
+			file.open(filename); file << knowledge.generate_dotfile(); file.close();
+
+			// create query-tree
+			knowledgebase<ANSWERTYPE> * query;
+			query = knowledge.create_query_tree();
+
+			snprintf(filename, 128, "knowledgebase%02d%c-q.dot", iteration, c);
+			file.open(filename); file << query->generate_dotfile(); file.close();
+
+			// answer queries
+			stats.query_count.uniq_membership += amore_alf_glue::automaton_answer_knowledgebase(*nfa, *query);
+
+			snprintf(filename, 128, "knowledgebase%02d%c-r.dot", iteration, c);
+			file.open(filename); file << query->generate_dotfile(); file.close();
+
+			// merge answers into knowledgebase
+			knowledge.merge_knowledgebase(*query);
+			delete query;
+			c++;
+		}
+
+		if(hypothesis)
+			delete hypothesis;
+		hypothesis = construct_amore_automaton(f_is_dfa, f_alphabet_size, f_state_count, f_initial, f_final, f_transitions);
+		if(!hypothesis) {
+			printf("generation of hypothesis failed!\n");
+			return -1;
+		}
+
+		{{{ /* dump/serialize table */
+			basic_string<int32_t> serialized;
+			basic_string<int32_t>::iterator it;
+
+			snprintf(filename, 128, "table%02d.text.angluin", iteration);
+			file.open(filename); ot.print(file); file.close();
+
+			/*
+			serialized = ot.serialize();
+
+			snprintf(filename, 128, "table%02d.serialized.angluin", iteration);
+			file.open(filename);
+
+			for(it = serialized.begin(); it != serialized.end(); it++) {
+				file << ntohl(*it);
+				file << ";";
+			}
+
+			file.close();
+			*/
+		}}}
+
+		snprintf(filename, 128, "hypothesis%02d.dot", iteration);
+		file.open(filename); file << hypothesis->generate_dotfile(); file.close();
+
+		printf("hypothesis %02d state count %02d\n", iteration, hypothesis->get_state_count());
+		if(hypothesis_state_count >= hypothesis->get_state_count()) {
+			log(LOGGER_ERROR, "STATE COUNT DID NOT INCREASE\n");
+			getchar();
+		}
+		hypothesis_state_count = hypothesis->get_state_count();
+
+		// once an automaton is generated, test for equivalence with oracle_automaton
+		// if this test is ok, all worked well
+
+		list<int> counterexample;
+		stats.query_count.equivalence++;
+		if(amore_alf_glue::automaton_equivalence_query(*nfa, *hypothesis, counterexample)) {
+			// equivalent
+			cout << "success.\n";
+			success = true;
+			break;
+		}
+
+		snprintf(filename, 128, "counterexample%02d.angluin", iteration);
+		file.open(filename);
+		print_word(file, counterexample);
+		ot.add_counterexample(counterexample);
+		file.close();
+	}
+
+	iteration++;
+	snprintf(filename, 128, "knowledgebase%02d-final.dot", iteration);
+	file.open(filename);
+	file << knowledge.generate_dotfile();
+	file.close();
+
+	ot.get_memory_statistics(stats);
+
+	delete nfa;
+
+	cout << "\nrequired membership queries: " << stats.query_count.membership << "\n";
+	cout << "required uniq membership queries: " << stats.query_count.uniq_membership << "\n";
+	cout << "required equivalence queries: " << stats.query_count.equivalence << "\n";
+	cout << "sizes: bytes: " << stats.table_size.bytes
+	     << ", members: " << stats.table_size.members
+	     << ", words: " << stats.table_size.words << "\n";
+	cout << "upper table rows: " << stats.table_size.upper_table
+	     << ", lower table rows: " << stats.table_size.lower_table
+	     << ", columns: " << stats.table_size.columns << "\n";
+	cout << "minimal state count: " << hypothesis->get_state_count() << "\n";
+
+	delete hypothesis;
+
+	if(success)
+		return 0;
+	else
+		return 2;
+}
+
