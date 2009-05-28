1c234d62aa901bd8d3c56575f1f3934efc586b4a
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-05-28 11:40:25 +0000

    basic data structures for KVtree...
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@629 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- include/libalf/algorithm_KVtree.h
+++ include/libalf/algorithm_KVtree.h
@@ -47,60 +47,61 @@ class KVtree: public learning_algorithm<answer> {
 
 		class candidate {
 			public:
-				typename knowledgebase<answer>::node * word;
-				node * position; // position in tree
-				bool is_state; // state candidate or just a transition?
+				node * current_position;
 			public:
-				candidate(typename knowledgebase<answer>::node *word, node *position, bool is_state)
-				{{{
-					this->word = word;
-					this->position = position;
-					this->is_state = is_state;
-				}}}
-				void make_state()
-				{{{
-					is_state = true;
-				}}}
+				candidate()
+				{ current_position = NULL };
 		};
+		class transition_dst : public candidate {
+			public:
+				leaf * src;
+				int label;
+			public:
+				transition_dst()
+				{ leaf = NULL; label = -1; };
+		};
+		class counterexample : public candidate {
+			public:
+				list<int> word;
+		};
+
 
 		class node {
 			public:
-				list<int> suffix; // splitting suffix of this node
-				bool is_leaf;
-				list<candidate *> candidates; // candidates (pending, if this is not a leaf)
-				node *accepting, *rejecting;
+				knowledgebase::node * word;
 			public:
-				node()
-				{{{
-					is_leaf = true;
-					accepting = NULL;
-					rejecting = NULL;
-				}}};
-				~node()
-				{{{
-					if(!is_leaf) {
-						delete accepting;
-						delete rejecting;
-					}
-				}}}
-				int get_memory_usage()
-				{{{
-					int ret = sizeof(this);
-					if(!is_leaf) {
-						ret += accepting->get_memory_usage();
-						ret += rejecting->get_memory_usage();
-					}
-					return ret;
-				}}}
+				virtual int get_memory_usage() = 0;
+				virtual ~node() { };
+		};
+		class internal : public node {
+			public:
+				node * accepting;
+				node * rejecting;
+			public:
+				virtual ~internal()
+				{ delete accepting; delete rejecting; }
+				virtual int get_memory_usage()
+				{ return sizeof(class internal) + accepting->get_memory_usage() + rejecting->get_memory_usage(); }
+		};
+		class leaf : public node {
+			public:
+				int state_index;
+				bool is_final;
+				list<transition_dst*> incoming;
+			public:
+				virtual ~leaf()
+				{ while(!incoming.empty()) { delete incoming.front(); incoming.pop_front(); } }
+				virtual int get_memory_usage()
+				{ return sizeof(class leaf) + sizeof(transition_dst) * incoming.size(); }
 		};
 
 	protected: // data
-		node * tree; // suffix-splitting tree
 
-		list<candidate*> pending; // all pending candidates (that have not yet reached a leaf in the tree)
-
-		map<typename knowledgebase<answer>::node*, candidate*> candidates; // ALL candidates
-		set<candidate*> final; // NOTE: also contains transitions going to final states. this is maintained in sift_pending().
+		// tree, pending candidates and all leaf-nodes in tree
+		node * tree;
+		list<candidate*> pending;
+		list<leaf*> leaves;
+		int current_state_count;
 
 		// last hypothesis:
 		bool lh_valid;
@@ -118,21 +119,20 @@ class KVtree: public learning_algorithm<answer> {
 			this->set_knowledge_source(base);
 			this->set_logger(log);
 
+			current_state_count = 0;
+
 			lh_valid = false;
 
-			tree = new node();
-			list<int> epsilon;
-			base->add_query(epsilon, 0);
-			new_candidate(base->get_nodeptr(epsilon), true);
+			tree = NULL;
 		}}}
 		virtual ~KVtree()
 		{{{
-			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
+			list<candidate>::iterator pi;
 
 			if(tree)
 				delete tree;
-			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
-				delete ci->second;
+			for(pi = pending.begin(); pi != pending.end(); ++pi)
+				delete *pi;
 		}}}
 
 		virtual void increase_alphabet_size(int new_asize)
@@ -140,20 +140,18 @@ class KVtree: public learning_algorithm<answer> {
 			if(new_asize <= this->get_alphabet_size())
 				return;
 
-			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
-
 			// add new transitions for all states.
-			for(ci = candidates.begin(); ci != candidates.end(); ++ci)
-				if(ci->second->is_state)
-					for(int new_suffix = this->get_alphabet_size(); new_suffix < new_asize; ++new_suffix)
-						new_candidate(ci->second->word->find_or_create_child(new_suffix), false);
+			list<leaf*>::iterator li;
+			for(li = leaves.begin; li != leaves.end(); ++li)
+				for(int new_suffix = this->get_alphabet_size(); new_suffix < new_asize; ++new_suffix)
+					new_candidate((*li)->word->find_or_create_child(new_suffix), false);
 
 			this->set_alphabet_size(new_asize);
 		}}}
 		virtual void get_memory_statistics(statistics & stats)
 		// table_size.words is an approximation!
 		{{{
-			stats.table_size.bytes = sizeof(this) + tree->get_memory_usage() + candidates.size() * sizeof(candidate);
+			stats.table_size.bytes = sizeof(this) + tree->get_memory_usage() + pending.size() * sizeof(candidate);
 		}}}
 		virtual bool sync_to_knowledgebase()
 		{{{
@@ -227,87 +225,14 @@ class KVtree: public learning_algorithm<answer> {
 		bool new_candidate(typename knowledgebase<answer>::node* word, bool is_state)
 		// create new candidate or make existing a state candidate.
 		// returns false if candidate was already known.
-		{{{
-			typename map<typename knowledgebase<answer>::node*, candidate*>::iterator ci;
-			ci = candidates.find(word);
-
-			if(ci == candidates.end()) {
-				// new candidate
-				candidate * c = new candidate(word, tree, is_state);
-				tree->candidates.push_back(c);
-				if(!tree->is_leaf)
-					pending.push_back(c);
-				candidates[word] = c;
-				// add transitions
-				if(is_state)
-					for(int sigma = 0; sigma < this->get_alphabet_size(); sigma++)
-						new_candidate(word->find_or_create_child(sigma), false);
-
-				return true;
-			} else {
-				// candidate already exists
-				if(is_state && !ci->second->is_state) {
-					// make it a state.
-					ci->second->is_state = true;
-					// add transitions
-					for(int sigma = 0; sigma < this->get_alphabet_size(); sigma++)
-						new_candidate(word->find_or_create_child(sigma), false);
-				}
-
-				return false;
-			}
-		}}}
+		{
+			
+		}
 
 		bool sift_pending()
-		{{{
-			if(pending.size() == 0)
-				return true;
-
-			bool new_required = false;
-			typename list<candidate*>::iterator pi;
-
-			for(pi = pending.begin(); pi != pending.end(); ++pi) {
-				typename knowledgebase<answer>::node *w;
-				bool sift_done = (*pi)->position->is_leaf;
-
-				while(!sift_done) {
-					// get word from prefix and new suffix
-					w = (*pi)->word->find_or_create_child((*pi)->position->suffix.begin(), (*pi)->position->suffix.end());
-					if(w->is_answered()) {
-						// move candidate to subtree
-						typename list<candidate*>::iterator d;
-						for(d = (*pi)->position->candidates.begin(); d != (*pi)->position->candidates.end(); ++d)
-							if(*d == *pi)
-								break;
-						(*pi)->position->candidates.erase( d );
-
-						// mark final-status now.
-						if((*pi)->position == tree)
-							if(w->get_answer() == true)
-								final.insert(*pi);
-
-						if(w->get_answer() == true)
-							(*pi)->position = (*pi)->position->accepting;
-						else
-							(*pi)->position = (*pi)->position->rejecting;
-
-						(*pi)->position->candidates.push_back(*pi);
-						if((*pi)->position->is_leaf) {
-							sift_done = true;
-							pi = pending.erase(pi);
-						};
-					} else {
-						// unable to sift until we're at a leaf.
-						// a teacher needs to answer the queries in the knowledgebase.
-						w->mark_required();
-						new_required = true;
-						sift_done = true;
-					}
-				}
-			}
-
-			return new_required;
-		}}}
+		{
+			
+		}
 
 		virtual bool complete()
 		{ return sift_pending(); }
