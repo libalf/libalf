b24281bf02c29c5931c2c219369f17aa78c1c287
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-07-05 22:04:06 +0000

    basic_string stuff i missed earlier; TODO
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@641 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- include/libalf/basic_string.h
+++ include/libalf/basic_string.h
@@ -23,6 +23,10 @@ void print_basic_string(basic_string<int32_t> str, ostream &os);
 
 void print_basic_string_2hl(basic_string<int32_t> str, ostream &os);
 
+bool basic_string_to_file(basic_string<int32_t> &str, const char* filename);
+
+bool file_to_basic_string(const char* filename, basic_string<int32_t> &str);
+
 }; // end of namespace libalf
 
 #endif // __libalf_basic_string_h__
--- src/basic_string.cpp
+++ src/basic_string.cpp
@@ -10,8 +10,15 @@
  */
 
 #include <ostream>
+#include <fstream>
 #include <string>
+
 #include <arpa/inet.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "libalf/basic_string.h"
 
@@ -20,7 +27,7 @@ namespace libalf {
 using namespace std;
 
 void print_basic_string(basic_string<int32_t> str, ostream &os)
-{
+{{{
 	basic_string<int32_t>::iterator si;
 
 	os << "_";
@@ -29,10 +36,10 @@ void print_basic_string(basic_string<int32_t> str, ostream &os)
 		os << *si;
 		os << "_";
 	}
-}
+}}}
 
 void print_basic_string_2hl(basic_string<int32_t> str, ostream &os)
-{
+{{{
 	basic_string<int32_t>::iterator si;
 
 	os << "_";
@@ -41,7 +48,48 @@ void print_basic_string_2hl(basic_string<int32_t> str, ostream &os)
 		os << ntohl(*si);
 		os << "_";
 	}
-}
+}}}
+
+bool basic_string_to_file(basic_string<int32_t> &str, const char* filename)
+{{{
+	int fd;
+	basic_string<int32_t>::iterator si;
+
+	fd = open(filename, O_WRONLY | O_CREAT);
+
+	if(fd < 0)
+		return false;
+
+	for(si = str.begin(); si != str.end(); si++) {
+		int32_t d = *si;
+		if(write(fd, &d, sizeof(d)) != sizeof(d))
+			return false;
+	}
+
+	return (close(fd) >= 0);
+}}}
+
+bool file_to_basic_string(const char* filename, basic_string<int32_t> &str)
+{{{
+	int fd;
+	basic_string<int32_t>::iterator si;
+	str.clear();
+
+	fd = open(filename, O_RDONLY);
+
+	if(fd < 0)
+		return false;
+
+	int32_t d;
+	int s;
+	while((s = read(fd, &d, sizeof(d))) == sizeof(d)) {
+		str.push_back(d);
+	}
+	if(s > 0)
+		return false;
+
+	return (close(fd) >= 0);
+}}}
 
 }; // end of namespace libalf
 
