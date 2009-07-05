a9050e17186d3866a7e42a68ec63e12b0bd85f6c
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-07-05 14:53:28 +0000

    testsuite for finding automata where NLstar needs as many eq queries aus automaton is big.
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@637 ee1db7db-6c55-0410-a809-e4d5348c71a5

---
+++ testsuites/NLstar_count_eq_queries/count_eq_queries.cpp
@@ -0,0 +1,133 @@
+/* $Id$
+ * vim: fdm=marker
+ *
+ * libalf - Automata Learning Factory
+ *
+ * (c) by David R. Piegdon, i2 Informatik RWTH-Aachen
+ *        <david-i2@piegdon.de>
+ *
+ * see LICENSE file for licensing information.
+ */
+
+#include <iostream>
+#include <ostream>
+#include <iterator>
+#include <fstream>
+#include <algorithm>
+
+#include <libalf/alf.h>
+#include <libalf/algorithm_NLstar.h>
+
+#include <amore++/nondeterministic_finite_automaton.h>
+#include <amore++/deterministic_finite_automaton.h>
+
+#include <LanguageGenerator/DFAenumerator.h>
+
+#include "amore_alf_glue.h"
+
+//#define ANSWERTYPE extended_bool
+#define ANSWERTYPE bool
+
+using namespace std;
+using namespace libalf;
+using namespace amore;
+using namespace LanguageGenerator;
+
+ostream_logger log(&cout, LOGGER_DEBUG);
+
+int learn_via_NLstar(int asize, finite_automaton * model)
+{{{
+	statistics stats;
+	knowledgebase<ANSWERTYPE> knowledge;
+	knowledge.set_statistics(&stats);
+
+	int iteration;
+	bool success = false;
+
+	// create NLstar table and teach it the automaton
+	NLstar_table<ANSWERTYPE> ot(&knowledge, &log, asize);
+	finite_automaton * hypothesis = NULL;
+
+	for(iteration = 1; iteration <= 100; iteration++) {
+		bool f_is_dfa;
+		int f_alphabet_size, f_state_count;
+		set<int> f_initial, f_final;
+		multimap<pair<int, int>, int> f_transitions;
+
+		while( ! ot.advance(f_is_dfa, f_alphabet_size, f_state_count, f_initial, f_final, f_transitions) )
+			stats.query_count.uniq_membership += amore_alf_glue::automaton_answer_knowledgebase(*model, knowledge);
+
+		if(hypothesis)
+			delete hypothesis;
+		hypothesis = construct_amore_automaton(f_is_dfa, f_alphabet_size, f_state_count, f_initial, f_final, f_transitions);
+		if(!hypothesis) {
+			cout << "generation of hypothesis failed!\n";
+			return -1;
+		}
+
+		list<int> counterexample;
+		stats.query_count.equivalence++;
+		if(amore_alf_glue::automaton_equivalence_query(*model, *hypothesis, counterexample)) {
+			// equivalent
+			success = true;
+			break;
+		}
+
+		ot.add_counterexample(counterexample);
+	}
+
+	if(hypothesis)
+		delete hypothesis;
+
+	if(success) {
+//		cout << "success.\n";
+		return stats.query_count.equivalence;
+	} else {
+		cout << "failed!\n";
+		return -1;
+	}
+}}}
+
+int main(int argc, char**argv)
+{{{
+	bool f_is_dfa;
+	int f_asize, f_state_count;
+	set<int> f_initial, f_final;
+	multimap<pair<int,int>, int> f_transitions;
+	int num = 0;
+
+	int asize = 2;
+	for(int size = 2; size < 5; size++) {
+		cout << "==================================== SIZE " << size << "\n";
+		DFAenumerator denum(size, asize);
+		while (!denum.generation_completed()) {
+			denum.next(true); // (we also want to skip the initial automaton)
+			denum.derive(f_is_dfa, f_asize, f_state_count, f_initial, f_final, f_transitions);
+			finite_automaton *model = construct_amore_automaton(f_is_dfa, f_asize, f_state_count, f_initial, f_final, f_transitions);
+			model->minimize();
+
+			if(model->get_state_count() < size) {
+//				printf("skipped: %d<%d; ", model->get_state_count(), size);
+				cout << ".";
+				free(model);
+				continue;
+			}
+//			cout << "\nmatch:\t\t";
+			cout << "+";
+
+			int eq_queries = learn_via_NLstar(asize, model);
+
+			if(eq_queries >= size) {
+				char filename[128];
+				ofstream file;
+				snprintf(filename, 128, "hit-%02d.dot", num);
+				file.open(filename); file << model->generate_dotfile(); file.close();
+				log(LOGGER_INFO, "match found with asize %d, state count %d, eq queries %d. saved as %s.\n",
+						asize, size, eq_queries, filename);
+				num++;
+			}
+			free(model);
+		}
+	}
+}}}
+
