54261b83dda703c64653b0a8d998ca0c53ffc55c
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-07-31 12:16:46 +0000

    FAT cleanup
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@713 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- finite-automata-tool/output.cpp
+++ finite-automata-tool/output.cpp
@@ -11,9 +11,13 @@
 
 #include <iostream>
 
+#include <unistd.h>
+
 #include "fat.h"
 
 #include <libalf/basic_string.h>
+
+bool generate_samples(finite_automaton * automaton, knowledgebase<bool> & base, string sampletype);
 
 bool write_output(finite_automaton *& automaton, output out, string sampletype)
 {
@@ -32,16 +36,25 @@ bool write_output(finite_automaton *& automaton, output out, string sampletype)
 			return true;
 		case output_human_readable:
 //			cout << automaton.write();
-			break;
+			
+			return false;
 		case output_sample:
 		case output_sample_text:
 			knowledgebase<bool> base;
 
 			// FIXME: generate knowledgebase from automaton
-			
+			if(!generate_samples(automaton, base, sampletype)) {
+				cerr << "failed to generate samples\n";
+				return false;
+			}
 
 			if(out == output_sample) {
-				
+				basic_string<int32_t> serial;
+				serial = base.serialize();
+				if(!basic_string_to_fd(serial, STDOUT_FILENO)) {
+					cerr << "failed to write serial\n";
+					return false;
+				}
 			} else {
 				if(out == output_sample_text) {
 					cout << base.generate_dotfile();
@@ -51,6 +64,12 @@ bool write_output(finite_automaton *& automaton, output out, string sampletype)
 			return true;
 	}
 
+	return false;
+}
+
+bool generate_samples(finite_automaton * automaton, knowledgebase<bool> & base, string sampletype)
+{
+	
 	return false;
 }
 
--- finite-automata-tool/transformation.cpp
+++ finite-automata-tool/transformation.cpp
@@ -18,32 +18,27 @@ bool do_transformation(finite_automaton *& automaton, transformation trans)
 	finite_automaton * tmp;
 	switch(trans) {
 		case trans_none:
-cerr<<"T:none\n";
 			return true;
 		case trans_mdfa:
-cerr<<"T:mdfa\n";
 			tmp = automaton->determinize();
 			delete automaton;
 			automaton = tmp;
 			/* fall through */
 		case trans_minimize:
-cerr<<"T:min\n";
 			automaton->minimize();
 			return true;
 		case trans_determinize:
-cerr<<"T:det\n";
 			tmp = automaton->determinize();
 			delete automaton;
 			automaton = tmp;
 			return true;
 		case trans_rfsa:
-cerr<<"T:rfsa\n";
 			// FIXME: apply NL*
-
+			
 			break;
 		case trans_universal:
-cerr<<"T:uni\n";
 			// FIXME: apply UL*
+			
 			break;
 	}
 
