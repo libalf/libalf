9b104b17589d4a790778d65ddb32bd364fa0b46b
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-08-08 00:58:41 +0000

    human readable automaton format parser
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@729 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -21,6 +21,8 @@
 #include <arpa/inet.h>
 #endif
 
+#include <stdlib.h>
+
 #include <libalf/automaton.h>
 
 namespace libalf {
@@ -106,13 +108,254 @@ string automaton2dotfile(int alphabet_size, int state_count, set<int> & initial,
 }}}
 
 
+static string parser_split_line(string & blob)
+{{{
+	int p;
+	string tmp, ret;
 
-bool read_automaton(string input, bool is_dfa, int & alphabet_size, int & state_count, set<int> & initial, set<int> & final, multimap<pair<int, int>, int> & transitions)
-{
-	
-}
+	// split first line off.
+	p = blob.find_first_of('\n');
+	tmp = blob.substr(0, p);
+	blob = blob.substr(p+1, string::npos);
 
-string write_automaton(bool is_dfa, int & alphabet_size, int & state_count, set<int> & initial, set<int> & final, multimap<pair<int, int>, int> & transitions)
+	// remove comments
+	tmp = tmp.substr(0, tmp.find_first_of('#'));
+
+	// strip superficial whitespace et al
+	bool front=true;
+	bool white=false;
+	for(size_t i = 0; i < tmp.size(); i++) {
+		switch (tmp[i]) {
+			case ' ':
+			case '\f':
+			case '\n':
+			case '\r':
+			case '\t':
+			case '\v':
+				if(!front && !white) {
+					ret.push_back(' ');
+					white = true;
+				}
+				break;
+			case ';':
+			case '=':
+			case ',':
+			case '[':
+			case ']':
+				if(front) {
+					ret.push_back(tmp[i]);
+				} else {
+					if(white) {
+						ret[ret.size()-1] = tmp[i];
+					} else {
+						ret.push_back(tmp[i]);
+					}
+				}
+				front = true;
+				white = false;
+				break;
+			default:
+				if(isupper(tmp[i]))
+					ret.push_back(tmp[i] - 'A' + 'a');
+				else
+					ret.push_back(tmp[i]);
+				front = false;
+				white = false;
+				break;
+		}
+	}
+
+	// remove white-space at EOL
+	if(white)
+		ret = ret.substr(0, ret.size()-1);
+
+	// if this line is empty, try next line
+	if(ret.empty() && !blob.empty())
+		return parser_split_line(blob);
+	else
+		return ret;
+}}}
+static bool parser_get_set(string single, set<int>& s)
+{{{
+	while(1) {
+		string num;
+		size_t p;
+		p = single.find_first_of(',');
+		num = single.substr(0, p);
+		if(p != string::npos) {
+			single = single.substr(p+1, string::npos);
+		} else {
+			single="";
+		}
+		if(num == "") {
+			break;
+		} else {
+			int n;
+			char * endptr;
+			n = strtol(num.c_str(), &endptr, 10);
+			if(*endptr)
+				return false;
+			s.insert(n);
+		}
+	}
+
+	return true;
+}}}
+static bool parser_get_transition(string single, multimap<pair<int, int>, int> & transitions)
+{{{
+	string tok;
+	size_t p;
+	char *endptr;
+	pair<pair<int, int>, int> tr;
+
+	// FROM
+	p = single.find_first_of(',');
+	tok = single.substr(0,p);
+	if(p == string::npos)
+		return false;
+	single = single.substr(p+1, string::npos);
+
+	tr.first.first = strtol(tok.c_str(), &endptr, 10);
+	if(*endptr)
+		return false;
+
+	// LABEL
+	p = single.find_first_of(',');
+	tok = single.substr(0,p);
+	if(p == string::npos)
+		return false;
+	single = single.substr(p+1, string::npos);
+
+	tr.first.second = strtol(tok.c_str(), &endptr, 10);
+	if(*endptr)
+		return false;
+
+	// TO
+	tok = single;
+
+	tr.second = strtol(tok.c_str(), &endptr, 10);
+	if(*endptr)
+		return false;
+
+	transitions.insert(tr);
+
+	return true;
+}}}
+
+bool read_automaton(string input, bool & is_dfa, int & alphabet_size, int & state_count, set<int> & initial, set<int> & final, multimap<pair<int, int>, int> & transitions)
+{{{
+	bool set_is_dfa = false, set_alphabet_size = false, set_state_count = false;
+	enum section {
+		section_none,
+		section_general,
+		section_initial,
+		section_final,
+		section_transitions
+	} current_section = section_none;
+
+	// split lines
+	string line, single, token;
+	while(1) {
+		line = parser_split_line(input);
+		if(input.empty())
+			break;
+
+		// split semi-colons
+		while(1) {
+			size_t p;
+			p = line.find_first_of(';');
+			single = line.substr(0, p);
+			line = (p == string::npos) ? "" : line = line.substr(p+1, string::npos);
+
+			if(single.empty()) {
+				if(line.empty())
+					break;
+			} else {
+//				printf("new single >>%s<<\n", single.c_str());
+				if(single[0] == '[') {
+					if(single[single.size()-1] != ']')
+						return false;
+
+					single = single.substr(1, single.size()-2);
+					if(single == "general")
+						current_section = section_general;
+					else
+						if(single == "initial states")
+							current_section = section_initial;
+						else
+							if(single == "final states")
+								current_section = section_final;
+							else
+								if(single == "transitions")
+									current_section = section_transitions;
+								else
+									return false;
+				} else {
+					switch (current_section) {
+						case section_none:
+							return false;
+						case section_general:
+							{
+								string lval, rval;
+								char *endptr;
+								size_t p = single.find_first_of('=');
+								if(p == string::npos)
+									return false;
+								lval = single.substr(0,p);
+								rval = single.substr(p+1, string::npos);
+								if(lval == "is dfa") {
+									if(rval == "true" || rval == "yes") {
+										is_dfa = true;
+										set_is_dfa = true;
+									} else {
+										if(rval == "false" || rval == "no") {
+											is_dfa = false;
+											set_is_dfa = true;
+										} else {
+											return false;
+										}
+									}
+								} else {
+									if(lval == "alphabet size") {
+										set_alphabet_size = true;
+										alphabet_size = strtol(rval.c_str(), &endptr, 10);
+										if(*endptr)
+											return false;
+									} else {
+										if(lval == "number of states") {
+											set_state_count = true;
+											state_count = strtol(rval.c_str(), &endptr, 10);
+											if(*endptr)
+												return false;
+										} else {
+											return false;
+										}
+									}
+								}
+							}
+							break;
+						case section_initial:
+							if(!parser_get_set(single, initial))
+								return false;
+							break;
+						case section_final:
+							if(!parser_get_set(single, final))
+								return false;
+							break;
+						case section_transitions:
+							if(!parser_get_transition(single, transitions))
+								return false;
+							break;
+					}
+				}
+			}
+		}
+	}
+
+	return (set_is_dfa && set_alphabet_size && set_state_count);
+}}}
+
+string write_automaton(bool is_dfa, int alphabet_size, int state_count, set<int> & initial, set<int> & final, multimap<pair<int, int>, int> & transitions)
 {{{
 	string ret;
 	char buf[256];
@@ -304,21 +547,6 @@ bool automaton_is_deterministic(int alphabet_size, int state_count, set<int> & i
 	return true;
 }}}
 
-static bool intersection_empty(set<int> & set1, set<int> & set2)
-{{{
-	set<int>::iterator s1, s2;
-	for(s1 = set1.begin(), s2 = set2.begin(); s1 != set1.end() && s2 != set2.end(); /* nothing */) {
-		if(*s1 == *s2)
-			return false;
-		if(*s1 < *s2)
-			s1++;
-		else
-			s2++;
-	}
-
-	return true;
-}}}
-
 void epsilon_closure(set<int> & states, multimap<pair<int, int>, int> & transitions)
 {{{
 	set<int>::iterator sti;
@@ -340,6 +568,22 @@ void epsilon_closure(set<int> & states, multimap<pair<int, int>, int> & transiti
 					new_states.push(tri->second);
 				}
 	}
+}}}
+
+/*
+static bool intersection_empty(set<int> & set1, set<int> & set2)
+{{{
+	set<int>::iterator s1, s2;
+	for(s1 = set1.begin(), s2 = set2.begin(); s1 != set1.end() && s2 != set2.end(); ;;;) {
+		if(*s1 == *s2)
+			return false;
+		if(*s1 < *s2)
+			s1++;
+		else
+			s2++;
+	}
+
+	return true;
 }}}
 
 bool simulate_automaton(set<int> & current, int label,   set<int> & final, multimap<pair<int, int>, int> & transitions, bool check_epsilon_transitions)
@@ -382,6 +626,7 @@ bool simulate_automaton(set<int> & current, list<int>::iterator word_begin, list
 
 	return !intersection_empty(current, final);
 }}}
+*/
 
 }; // end of namespace libalf
 
