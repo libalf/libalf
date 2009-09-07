999c8c0a99a3311e93bba14a83b7f962a312fc4f
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-09-07 13:14:30 +0000

    different method of choosing counterexamples (amore-alf-glue)
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@836 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- glue/amore_alf_glue.h
+++ glue/amore_alf_glue.h
@@ -26,11 +26,30 @@ using namespace amore;
 
 inline bool automaton_equivalence_query(finite_automaton & automaton, finite_automaton & hypothesis, list<int> & counterexample)
 {
+	// there are two options:
+	// either get automaton containing the symmetric difference of model and hypothesis (variant 1)
+	// or first get model \ hypothesis and if this is empty, get hypothesis \ model (variant 2).
+	// variant 1 seems more logically, but variant 2 seems to get better result.
+
 	finite_automaton * difference;
 	bool is_empty;
 
+#if 0
+	// variant 1:
 	difference = automaton.lang_symmetric_difference(hypothesis);
 	counterexample = difference->get_sample_word(is_empty);
+#else
+	// variant 2:
+	difference = automaton.lang_difference(hypothesis);
+	counterexample = difference->get_sample_word(is_empty);
+
+	if(is_empty) {
+		delete difference;
+		difference = hypothesis.lang_difference(automaton);
+		counterexample = difference->get_sample_word(is_empty);
+	}
+#endif
+
 	delete difference;
 
 	return is_empty;
