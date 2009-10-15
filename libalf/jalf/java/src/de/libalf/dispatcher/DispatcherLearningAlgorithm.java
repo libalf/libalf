2fcef9ba1c644ec91209dc75f04dd46ac785aaf6
stefanschulz <stefanschulz@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-10-15 07:21:44 +0000

    comment revival
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@1015 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- libalf/jalf/java/src/de/libalf/dispatcher/DispatcherLearningAlgorithm.java
+++ libalf/jalf/java/src/de/libalf/dispatcher/DispatcherLearningAlgorithm.java
@@ -1,15 +1,23 @@
 /*
- * This file is part of libalf. libalf is free software: you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version. libalf is distributed in the hope that
- * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details. You should have received a
- * copy of the GNU Lesser General Public License along with libalf. If not, see
- * <http://www.gnu.org/licenses/>. (c) 2009 Lehrstuhl Softwaremodellierung und
- * Verifikation (I2), RWTH Aachen University and Lehrstuhl Logik und Theorie
- * diskreter Systeme (I7), RWTH Aachen University Author: Stefan Schulz
+ * This file is part of libalf.
+ *
+ * libalf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libalf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libalf.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (c) 2009 Lehrstuhl Softwaremodellierung und Verifikation (I2), RWTH Aachen University
+ *           and Lehrstuhl Logik und Theorie diskreter Systeme (I7), RWTH Aachen University
+ * Author: Stefan Schulz
+ *
  */
 
 package de.libalf.dispatcher;
