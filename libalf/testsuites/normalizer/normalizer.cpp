c40674b53a2f42fe45dc7967e19acf7bc3f15041
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2009-10-28 10:55:22 +0000

    better testcase for normalizer
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@1053 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- libalf/testsuites/normalizer/normalizer.cpp
+++ libalf/testsuites/normalizer/normalizer.cpp
@@ -32,28 +32,30 @@
 using namespace std;
 using namespace libalf;
 
-// alphabet:
-//
-// /message type	/letter
-//
-// a->b(0)		0,1
-// a->b(1)		2,3
-// a->c(0)		4,5
-// a->c(1)		6,7
-//
-// b->a(0)		8,9
-// b->a(1)		10,11
-// b->c(0)		12,13
-// b->c(1)		14,15
-//
-// c->a(0)		16,17
-// c->a(1)		18,19
-// c->b(0)		20,21
-// c->b(1)		22,23
 
 normalizer * create_normalizer()
 {{{
-	int i;
+// NOTE: this is the OLD normalizer that is commented out.
+	// alphabet:
+	//
+	// /message type	/letter
+	//
+	// a->b(0)		0,1
+	// a->b(1)		2,3
+	// a->c(0)		4,5
+	// a->c(1)		6,7
+	//
+	// b->a(0)		8,9
+	// b->a(1)		10,11
+	// b->c(0)		12,13
+	// b->c(1)		14,15
+	//
+	// c->a(0)		16,17
+	// c->a(1)		18,19
+	// c->b(0)		20,21
+	// c->b(1)		22,23
+
+	unsigned int i;
 
 	vector<int> total_order;
 	vector<int> process_match;
@@ -61,21 +63,37 @@ normalizer * create_normalizer()
 	int max_buffer_length;
 
 	// total order
-	int v_total_order[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
-	for(i = 0; i < 24; i++)
+	printf("total order:   ");
+//	int v_total_order[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+	int v_total_order[] = {0,1,2,3,4,5};
+	for(i = 0; i < sizeof(v_total_order)/sizeof(int); i++) {
+		printf("%d ", v_total_order[i]);
 		total_order.push_back(v_total_order[i]);
+	}
+	printf(";\n");
 
 	// process match
-	int v_process_match[] = {0,1, 0,1, 0,2, 0,2, 1,0, 1,0, 1,2, 1,2, 2,0, 2,0, 2,1, 2,1};
-	for(i = 0; i < 24; i++)
+	printf("process match: ");
+//	int v_process_match[] = {0,1, 0,1, 0,2, 0,2, 1,0, 1,0, 1,2, 1,2, 2,0, 2,0, 2,1, 2,1};
+	int v_process_match[] = {0,1,1,0,0,1};
+	for(i = 0; i < sizeof(v_process_match)/sizeof(int); i++) {
+		printf("%d ", v_process_match[i]);
 		process_match.push_back(v_process_match[i]);
+	}
+	printf(";\n");
 
 	// buffer match
-	int v_buffer_match[] = {0,0, 0,0, 1,1, 1,1, 2,2, 2,2, 3,3, 3,3, 4,4, 4,4, 5,5, 5,5};
-	for(i = 0; i < 24; i++)
+//	int v_buffer_match[] = {0,0, 0,0, 1,1, 1,1, 2,2, 2,2, 3,3, 3,3, 4,4, 4,4, 5,5, 5,5};
+	int v_buffer_match[] = {0,0,1,1,0,0};
+	printf("buffer match:  ");
+	for(i = 0; i < sizeof(v_buffer_match)/sizeof(int); i++) {
+		printf("%d ",v_buffer_match[i]);
 		buffer_match.push_back(v_buffer_match[i]);
+	}
+	printf(";\n");
 
 	max_buffer_length = 1;
+	printf("buffer length: %d\n\n\n", max_buffer_length);
 
 	return new normalizer_msc(total_order, process_match, buffer_match, max_buffer_length);
 }}}
@@ -84,7 +102,7 @@ list<int> create_word()
 {{{
 	list<int> ret;
 	unsigned int i;
-	int v_word[] = {0,2,4,4,5,0,1,3,1,5};
+	int v_word[] = {0, 1, 2, 3, 4, 5, 2, 4, 3, 5};
 
 	for(i = 0; i < sizeof(v_word)/sizeof(int); i++)
 		ret.push_back(v_word[i]);
@@ -92,7 +110,17 @@ list<int> create_word()
 	return ret;
 }}}
 
-int main()
+list<int> read_word(int argc, char**argv)
+{{{
+	list<int> ret;
+
+	for(int i = 1; i < argc; i++)
+		ret.push_back(atoi(argv[i]));
+
+	return ret;
+}}}
+
+int main(int argc, char**argv)
 {{{
 	normalizer * norm;
 	list<int> word, pnf, snf;
@@ -100,7 +128,7 @@ int main()
 
 	norm = create_normalizer();
 
-	word = create_word();
+	word = read_word(argc, argv);
 
 	cout << "word: ";
 	print_word(cout, word);
