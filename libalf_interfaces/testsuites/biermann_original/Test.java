4ce5e76ff733c746913e63b6aa4884f66fcc5d39
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2011-02-28 17:16:10 +0000

    testcase more general, easier to pick JNI or Dispatcher.
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@1448 ee1db7db-6c55-0410-a809-e4d5348c71a5

--- libalf_interfaces/testsuites/biermann_original/Test.java
+++ libalf_interfaces/testsuites/biermann_original/Test.java
@@ -1,27 +1,34 @@
 /* $Id$ */
 /* test some functionality of dispatcher */
 
-import de.libalf.LibALFFactory.Algorithm;
-import de.libalf.dispatcher.DispatcherFactory;
 import de.libalf.Logger;
 import de.libalf.Knowledgebase;
 import de.libalf.LearningAlgorithm;
 
+import de.libalf.LibALFFactory;
+import de.libalf.LibALFFactory.Algorithm;
+
+import de.libalf.dispatcher.DispatcherFactory;
+import de.libalf.jni.JNIFactory;
+
+
 public class Test {
 
 	public static void main(String[] args) {
-		// Get factory
-		DispatcherFactory f = new DispatcherFactory("localhost", 24940);
+		/* Get factory (pick either jni or dispatcher) */
+		LibALFFactory f = new DispatcherFactory("localhost", 24940);
+//		LibALFFactory f = JNIFactory.STATIC;
 
-		// Use factory to get our objects
+		/* Use factory to get our objects */
 		Logger log = f.createLogger();
 		Knowledgebase base = f.createKnowledgebase();
 		LearningAlgorithm alg = f.createLearningAlgorithm(Algorithm.BIERMANN_ORIGINAL, base, 2, 2);
 		alg.set_logger(log);
 
-		// Do something
+		/* Do something */
+//		nothing TODO here.
 
-		// Clean up and get logger content
+		/* Clean up and get logger content */
 		alg.remove_logger();
 		alg = null;
 		base = null;
