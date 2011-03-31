36011862f4eb6a0bc95f1f45b38d7d8607180064
davidpiegdon <davidpiegdon@ee1db7db-6c55-0410-a809-e4d5348c71a5>
2011-03-31 23:03:06 +0000

    libalf: clean PREFIX handling, also in testcases
    git-svn-id: https://svn-i2.informatik.rwth-aachen.de/repos/libalf/trunk@1472 ee1db7db-6c55-0410-a809-e4d5348c71a5

---
+++ libalf/config.mk
@@ -0,0 +1,13 @@
+# $Id$
+# vim:syntax=make
+#
+# directory definitions for Makefile
+
+PREFIX?=/usr/local
+LIBDIR?=${PREFIX}/lib
+INCLUDEDIR?=${PREFIX}/include
+BINDIR?=${PREFIX}/bin
+SHAREDIR?=${PREFIX}/share
+JARDIR?=${SHAREDIR}/libalf
+DOCDIR?=${SHAREDIR}/doc
+
